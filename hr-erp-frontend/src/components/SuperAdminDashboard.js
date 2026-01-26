79c8b7d989f87abc1a49c9492a82103ed7692219
davidZakaria <david150778@bue.edu.eg>
2026-01-26 18:26:27 +0200

    Fix missing axios import in SuperAdminDashboard

--- hr-erp-frontend/src/components/SuperAdminDashboard.js
+++ hr-erp-frontend/src/components/SuperAdminDashboard.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import LogoutButton from './LogoutButton';
 import { useTranslation } from 'react-i18next';
 import AttendanceManagement from './AttendanceManagement';
